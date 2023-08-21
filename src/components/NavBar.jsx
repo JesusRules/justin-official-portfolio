c6ae585f06686f46aa98365850ec52d203d41b13
JesusRules <36013969+JesusRules@users.noreply.github.com>
2023-08-20 22:00:21 -0400

    Menu drops doWn noW

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,11 +4,14 @@ import { styled } from 'styled-components'
 const Container = styled.div`
     display: flex;
     justify-content: center;
+    position: fixed;
+    left: 0;
+    right: 0;
+    z-index: 2;
     `
 const Image = styled.img`
-    z-index: 2;
     width: 100%;
-    min-width: 1460px;
+    min-width: 1500px;
     text-align: center;
     cursor: pointer;
     `
@@ -20,24 +23,80 @@ const DarkBG = styled.div`
     height: 100%;
     background-color: rgba(0, 0, 0, 0.5);
     cursor: pointer;
+    `
+
+const MenuContainer = styled.div`
+    position: fixed;
+    top: 40px;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 1;
+    width: 200px;
+    height: 280px;
+    `
+const MenuImage = styled.img`
+    width: inherit;
+    height: inherit;
+    object-fit: cover;
+    `
+const MenuItems = styled.ul`
+    background-color: rgba(0, 0, 0, .3);
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    color: #fff;
+    text-decoration: none;
+    list-style: none;
+    a {
+        font-weight:bold;
+        color: #fff;
+        font-size: 1.4rem;
+
+    }
 `
 
 function NavBar() {
-    const [backGroundEnabled, setBackGroundEnabled] = useState(false);
+    const [menuEnabled, setMenuEnabled] = useState(false);
 
     const handleClick = () => {
-        setBackGroundEnabled(!backGroundEnabled);
+        setMenuEnabled(!menuEnabled);
     }
 
   return (
     <>
-    {backGroundEnabled && (
-        <DarkBG onClick={() => handleClick()}>
-        </DarkBG>
-    )}
     <Container>
         <Image src="/img/NavBar.png" onClick={() => handleClick()}/>
     </Container>
+
+    {menuEnabled && (
+    <>
+        <DarkBG onClick={() => handleClick()} />
+        <MenuContainer>
+            <MenuImage src="/img/jesus-flag.png" />
+            <MenuItems>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Works</a>
+                </li>
+                <li>
+                    <a href="#">Projects</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </MenuItems>
+        </MenuContainer>
+    </>
+    )}
     </>
   )
 }
