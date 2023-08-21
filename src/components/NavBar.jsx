0c39ab77f7ee358be4d6c57351c8a917c6444f00
JesusRules <36013969+JesusRules@users.noreply.github.com>
2023-08-20 21:30:30 -0400

    Navbar implemented

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,44 @@
-import React from 'react'
+import React, { useState }from 'react'
 import { styled } from 'styled-components'
 
 const Container = styled.div`
     display: flex;
     justify-content: center;
-`
+    `
 const Image = styled.img`
+    z-index: 2;
     width: 100%;
-    min-width: 1440px;
+    min-width: 1460px;
     text-align: center;
+    cursor: pointer;
+    `
 
+const DarkBG = styled.div`
+    z-index: 1;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    cursor: pointer;
 `
 
 function NavBar() {
+    const [backGroundEnabled, setBackGroundEnabled] = useState(false);
+
+    const handleClick = () => {
+        setBackGroundEnabled(!backGroundEnabled);
+    }
+
   return (
+    <>
+    {backGroundEnabled && (
+        <DarkBG onClick={() => handleClick()}>
+        </DarkBG>
+    )}
     <Container>
-        <Image src="/img/NavBar.png" />
+        <Image src="/img/NavBar.png" onClick={() => handleClick()}/>
     </Container>
+    </>
   )
 }
 
