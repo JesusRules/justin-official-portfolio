e8e2bb5bcd0b6c9c409f9e4ceed3b5d0d9d80902
JesusRules <36013969+JesusRules@users.noreply.github.com>
2023-08-21 18:26:49 -0400

    Adjust speeds and delays of react elements

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,14 +15,11 @@ const Sky = styled.img`
     object-fit: cover;
     position: absolute;
     /* width: 1920px; */
-    /* width: 2500px; */
-    /* width: 3200px; */
     height: 1420px;
     /* top: 50%; */
     top: 2000px;
     left: 50%;
     transform: translate(-50%, -50%);
-    background-color: red;
     z-index: 1;
 `
   
@@ -35,70 +32,69 @@ const Sky = styled.img`
 //     z-index: 2;
 //     transform: translate(-50%, -50%);
 // `
-const Dove2 = styled.img`
-    /* -webkit-transform: scaleX(-1); */
-    position: absolute;
-    width: 156px;
-    top: calc(50% - 230px);
-    left: calc(50% - 686px);
-    /* top: 100px; */
-    z-index: 2;
-    transform: translate(-50%, -50%) scaleX(-1);
-`
+// const Dove2 = styled.img`
+//     /* -webkit-transform: scaleX(-1); */
+//     position: absolute;
+//     width: 156px;
+//     top: calc(50% - 230px);
+//     left: calc(50% - 686px);
+//     /* top: 100px; */
+//     z-index: 2;
+//     transform: translate(-50%, -50%) scaleX(-1);
+// `
   
 const CloudMain_Back = styled.img`
     position: absolute;
     width: 1700px;
-    top: calc(50% + 271px);
+    /* top: calc(50% + 271px); */
     left: calc(50% - 0px);
-    /* top: 100px; */
-    /* background-color: blue; */
+    top: 2000px;
     z-index: 3;
 `
   
 const CloudMain_Front = styled.img`
     position: absolute;
     width: 1560px;
-    top: calc(50% + 286px);
+    /* top: calc(50% + 286px); */
     left: calc(50% - 0px);
-    /* top: 100px; */
-    /* background-color: red;  */
+    top: 3600px;
     z-index: 4;
 `
+
   
 const CloudBG_4 = styled.img`
     position: absolute;
     width: 190px;
-    top: calc(50% - 99px);
+    /* top: calc(50% - 99px); */
     left: calc(50% - 536px);
-    /* top: 100px; */
+    top: 2000px;
     z-index: 5;
 `
   
 const CloudBG_3 = styled.img`
     position: absolute;
     width: 250px;
-    top: calc(50% - 250px);
+    /* top: calc(50% - 250px); */
     left: calc(50% - 150px);
-    /* top: 100px; */
+    top: 2000px;
     z-index: 6;
 `
   
 const CloudBG_2 = styled.img`
     position: absolute;
     width: 185px;
-    top: calc(50% - 225px);
+    /* top: calc(50% - 225px); */
     left: calc(50% + 530px);
-    /* top: 100px; */
+    top: 2000px;
     z-index: 7;
 `
   
 const CloudBG_1 = styled.img`
     position: absolute;
     width: 380px;
-    top: calc(50% - 125px);
+    /* top: calc(50% - 125px); */
     left: calc(50% - 950px);
-    /* top: 100px; */
+    top: 2000px;
     z-index: 8;
 `
   
@@ -114,40 +110,33 @@ const CloudBG_1 = styled.img`
 const JesusFlag2 = styled.img`
     position: absolute;
     width: 360px;
-    top: calc(50% + 102px);
+    /* top: calc(50% + 102px); */
     left: calc(50% - 340px);
-    /* top: 100px; */
+    top: 3000px;
     z-index: 9;
     @media only screen and (max-width: 700px) {
         left: calc(50% - 340px + 135px);
-        top: calc(50% + 102px + 10px);
+        /* top: calc(50% + 102px + 10px); */
+        top: 3000px;
     }
 `
-  
-const Pipe = styled.img`
-    position: absolute;
-    width: 124px;
-    top: calc(50% + 140px);
-    left: calc(50% + 433px);
-    /* top: 100px; */
-    z-index: 10;
-`
+
   
 const Haunter = styled.img`
     position: absolute;
     width: 169px;
-    top: calc(50% - 110px);
+    /* top: calc(50% - 110px); */
     left: calc(50% - 509px);
-    /* top: 100px; */
+    top: 2100px;
     z-index: 11;
 `
   
 const Boo = styled.img`
     position: absolute;
     width: 89px;
-    top: calc(50% - 122px);
+    /* top: calc(50% - 122px); */
     left: calc(50% + 485px);
-    /* top: 100px; */
+    top: 2100px;
     z-index: 12;
 `
   
@@ -176,13 +165,22 @@ const Justin_Txt = styled.img`
         width: 530px;
     }
 `
+
+const Pipe = styled.img`
+    position: absolute;
+    width: 124px;
+    /* top: calc(50% + 140px); */
+    left: calc(50% + 433px);
+    top: 2000px;
+    z-index: 10;
+`
   
 const HelloKitty = styled.img`
     position: absolute;
     width: 120px;
-    top: calc(50% + 150px);
+    /* top: calc(50% + 150px); */
     left: calc(50% - 485px);
-    /* top: 100px; */
+    top: 2000px;
     z-index: 15;
     @media only screen and (max-width: 700px) {
         left: calc(50% - 495px + 290px);
@@ -193,9 +191,9 @@ const HelloKitty = styled.img`
 const Toad = styled.img`
     position: absolute;
     width: 122px;
-    top: calc(50% + 180px);
+    /* top: calc(50% + 180px); */
     left: calc(50% + 373px);
-    /* top: 100px; */
+    top: 2400px;
     z-index: 16;
     @media only screen and (max-width: 700px) {
         left: calc(50% + 383px - 160px);
@@ -206,9 +204,9 @@ const Toad = styled.img`
 const Kart = styled.img`
     position: absolute;
     width: 271px;
-    top: calc(50% + 218px);
+    /* top: calc(50% + 218px); */
     left: calc(50% - 410px);
-    /* top: 100px; */
+    top: 2400px;
     z-index: 17;
     @media only screen and (max-width: 700px) {
         left: calc(50% - 420px + 240px);
@@ -219,9 +217,9 @@ const Kart = styled.img`
 const Joy = styled.img`
     position: absolute;
     width: 122px;
-    top: calc(50% + 232px);
+    /* top: calc(50% + 232px); */
     left: calc(50% + 281px);
-    /* top: 100px; */
+    top: 2800px;
     z-index: 18;
     @media only screen and (max-width: 700px) {
         left: calc(50% + 291px - 100px);
@@ -232,9 +230,9 @@ const Joy = styled.img`
 const Pikachu = styled.img`
     position: absolute;
     width: 153px;
-    top: calc(50% + 235px);
+    top: calc(50% + 235px)k;
     left: calc(50% - 259px);
-    /* top: 100px; */
+    top: 2800px;
     z-index: 19;
     @media only screen and (max-width: 700px) {
         left: calc(50% - 269px + 115px);
@@ -245,9 +243,9 @@ const Pikachu = styled.img`
 const Mario = styled.img`
     position: absolute;
     width: 153px;
-    top: calc(50% + 245px);
+    /* top: calc(50% + 245px); */
     left: calc(50% - 155px);
-    /* top: 100px; */
+    top: 3200px;
     z-index: 20;
     @media only screen and (max-width: 700px) {
         left: calc(50% - 165px + 30px);
@@ -257,9 +255,9 @@ const Mario = styled.img`
 const MiniJesus = styled.img`
     position: absolute;
     width: 206px;
-    top: calc(50% + 160px);
+    /* top: calc(50% + 160px); */
+    top: 3200px;
     left: calc(50% + 173px);
-    /* top: 100px; */
     z-index: 21;
     @media only screen and (max-width: 700px) {
         left: calc(50% + 183px - 35px);
@@ -269,9 +267,9 @@ const MiniJesus = styled.img`
 const Justin = styled.img`
     position: absolute;
     width: 235px;
-    top: calc(50% + 157px);
+    /* top: calc(50% + 157px); */
+    top: 3600px;
     left: calc(50% + 0px);
-    /* top: 100px; */
     z-index: 22;
 `    
 
