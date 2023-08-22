1442bc162b2b8c82ddb557c7915b740f723ff465
JesusRules <36013969+JesusRules@users.noreply.github.com>
2023-08-21 23:29:57 -0400

    Conveyour belt of apps showing

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,6 +6,9 @@ const Container = styled.div`
     background-color: #f2f2f2;
     height: 100vh;
     scroll-snap-align: start;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 `
 
 const LogoImage = styled.img`
@@ -18,6 +21,7 @@ function Skills() {
       Skills!!
       <InfiniteScrollerApps />
       <InfiniteScrollerMedia />
+      <InfiniteScrollerLanguages />
     </Container>
   )
 }
@@ -73,6 +77,48 @@ function InfiniteScrollerMedia() {
     return (
         <div className='logos-opposite'>
             <div className='logos-slide-opposite'>
+                <img src="/logos/app-development/adobe-xd.png" />
+                <img src="/logos/app-development/amplify.png" />
+                <img src="/logos/app-development/android-studio.jpg" />
+                <img src="/logos/app-development/asp.net.png" />
+                <img src="/logos/app-development/aws-ec2.png" />
+                <img src="/logos/app-development/aws-lambda.png" />
+                <img src="/logos/app-development/aws-logo.png" />
+                <img src="/logos/app-development/figma.png" />
+                <img src="/logos/app-development/firebase.jpg" />
+                <img src="/logos/app-development/flutter.png" />
+                <img src="/logos/app-development/gsap.png" />
+                <img src="/logos/app-development/hostinger.png" />
+                <img src="/logos/app-development/knockoutjs.png" />
+                <img src="/logos/app-development/mongodb.jpg" />
+                <img src="/logos/app-development/mysql.png" />
+                <img src="/logos/app-development/net.maui.jpg" />
+                <img src="/logos/app-development/netlify.png" />
+                <img src="/logos/app-development/nextjs.jpg" />
+                <img src="/logos/app-development/nodejs-express.webp" />
+                <img src="/logos/app-development/postgres.png" />
+                <img src="/logos/app-development/react-native.png" />
+                <img src="/logos/app-development/react-redux.png" />
+                <img src="/logos/app-development/react.jpg" />
+                <img src="/logos/app-development/supabase.png" />
+                <img src="/logos/app-development/tailwindcss.jpg" />
+                <img src="/logos/app-development/threejs.png" />
+                <img src="/logos/app-development/vercel.jpg" />
+                <img src="/logos/app-development/vite.jpg" />
+            </div>
+        </div>
+    )
+}
+
+function InfiniteScrollerLanguages() {
+    useEffect(() => {
+        var copy = document.querySelector(".logos-slide-2").cloneNode(true);
+        document.querySelector('.logos-2').appendChild(copy);
+    }, [])
+
+    return (
+        <div className='logos-2'>
+            <div className='logos-slide-2'>
                 <img src="/logos/app-development/adobe-xd.png" />
                 <img src="/logos/app-development/amplify.png" />
                 <img src="/logos/app-development/android-studio.jpg" />
