45e5f9ea176efb737095536aba25d6d27db99eb9
Justin Bernard <justin.bernard320@gmail.com>
2023-09-04 00:17:14 -0400

    Spent forever trying to adjust height and width on project popout info modals

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from 'react'
 import './App.css'
+import './Swiper.css'
 import Skills from './components/Skills'
 import Hero from './components/Hero'
 import Who from './components/Who'
--- src/components/smaller-components/ProjectInfoModal.jsx
+++ src/components/smaller-components/ProjectInfoModal.jsx
@@ -2,6 +2,13 @@ import React, { useState, useEffect, useRef } from 'react'
 import { styled, keyframes  } from 'styled-components'
 import gsap from 'gsap';
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+
 const ProjectInfoModalDiv = styled.div`
   height: 85%;
   width: 92%;
@@ -18,10 +25,13 @@ const ProjectInfoModalDiv = styled.div`
   cursor: auto;
   background-color: #e6fbff;
   color: black;
+  overflow-y: auto;
 
   h2 {
+    border-bottom: 1px solid black;
     padding: 1rem;
     text-align: center;
+    margin-bottom: 1rem;
   }
 `;
 
@@ -114,19 +124,88 @@ function ProjectInfoModal(props) {
   )
 }
 
+
+
+// import slide_image_1 from '/img/sky/sky-back.jpg';
+// import slide_image_2 from '/img/sky/sky-down.jpg';
+// import slide_image_3 from '/img/projects/ultimate-jesus-game-display.png';
+// import slide_image_4 from '/img/projects/pokithedog/pokithedog-2.png';
+// import slide_image_5 from '/img/sky/sky-right.jpg';
+// import slide_image_6 from '/img/sky/sky-up.jpg';
+
+import slide_image_1 from '/img/projects/pokithedog/pokithedog-2.png';
+import slide_image_2 from '/img/projects/pokithedog/pokithedog-2.png';
+import slide_image_3 from '/img/projects/pokithedog/pokithedog-2.png';
+import slide_image_4 from '/img/projects/pokithedog/pokithedog-2.png';
+import slide_image_5 from '/img/projects/pokithedog/pokithedog-2.png';
+import slide_image_6 from '/img/projects/pokithedog/pokithedog-2.png';
+
 function PokiTheDog() {
     return (
         <>
         <h2>PokiTheDog</h2>
         <div style={{textAlign: 'center'}}>
-            <p>A website designed to practice and promote my development abilities!</p>
-            <p>Features some early games I've designed!</p>
+            <p>This website was designed to practice and promote my development abilities!</p>
+            <p>It features some early games I've designed!</p>
         </div>
         <div style={{textAlign: 'center', margin: '1rem'}}>
             <a style={{fontSize: '1.2rem'}} href='https://pokithedog.com/' target="_blank">https://pokithedog.com/</a>
         </div>
-        <BackgroundBanner></BackgroundBanner>
-        <p style={{color: 'blue', margin: '0.5rem .8rem', fontStyle: 'italic', fontWeight: 400}}>Made using vanilla <b>JavaScript, CSS and HTML.</b></p>
+        
+        {/* <BackgroundBanner></BackgroundBanner> */}
+        <div className='swiper_container1'>
+        <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={'4'}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: -55,
+          depth: 130,
+          modifier: 6, //2.5
+        }}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container2"
+      >
+        <SwiperSlide>
+          <img src={slide_image_1} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_2} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_3} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_4} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_5} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_6} alt="slide_image" />
+        </SwiperSlide>
+
+        <div className="slider-controler">
+                {/* <div className="swiper-button-prev slider-arrow">
+                    <img className='arrow-svg' src="/svg/arrow-left.svg" />
+                </div>
+                <div className="swiper-button-next slider-arrow">
+                    <img className='arrow-svg' src="/svg/arrow-right.svg" />
+                </div> */}
+                <div className="swiper-pagination"></div>
+            </div>
+      </Swiper>
+        </div>
+        <p style={{textAlign: 'center', color: 'blue', margin: '0.5rem .8rem', fontStyle: 'italic', fontWeight: 400}}>Website was made using vanilla <b>JavaScript, CSS and HTML.</b></p>
         </>
     )
 }
