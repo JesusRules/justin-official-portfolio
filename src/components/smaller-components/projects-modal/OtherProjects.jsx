0cf1d0ea0a86de35f137b478baf4aa853460e3da
Justin Bernard <bern0241@algonquinlive.com>
2023-09-12 23:49:08 -0400

    Setup for Other Projects grid good

--- src/components/smaller-components/projects-modal/OtherProjects.jsx
+++ src/components/smaller-components/projects-modal/OtherProjects.jsx
@@ -28,54 +28,65 @@ const ContentContainer = styled.div`
 `;
 
 const ProjectDiv = styled.div`
-  display: flex;
-  padding: 1rem 4rem;
-  justify-content: center;
-  gap: 1rem;
-  margin-bottom: .8rem;
-  @media only screen and (max-width: 700px) {
-  }
-  
-  .image {
-    width: 50%;
-    display: flex;
+  display: grid;
+  margin: 0 auto;
+  width: 88%;
+  grid-template-columns: 1fr 1fr;
+  gap: 1.5rem;
+  row-gap: 2rem;
+  align-items: center;
+  grid-template-areas: 
+    'one two' 
+    'three four';
+
+  margin-bottom: 1rem;
+
+  .grid-item {
     img {
       width: 100%;
-      cursor: pointer;
-      border: 1px solid black;
-      box-shadow: 4px 4px 4px rgba(0,0,0,0.3);
     }
-  }
-  .image-right {
-    justify-content: start;
-  }
-  .image-left {
-    justify-content: end;
-  }
-  
-  .info {
-    padding-left: 0.5rem;
-    padding-right: 0.5rem;
-    width: 50%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    position: relative;
-    bottom: 2.25rem;
     h3 {
-      margin-bottom: 1.33rem;
-      cursor: pointer;
       color: blue;
-      text-decoration: underline;
       font-style: italic;
+      text-decoration: underline;
+      cursor: pointer;
+      margin-bottom: 1rem;
     }
   }
-  .text-left {
+  .info {
+    position: relative;
+    bottom: 1rem;
+  }
+  .left {
     text-align: right;
   }
-  .text-right {
-    text-align: left;
+  img {
+    border: 1px solid black;
+    box-shadow: 5px 5px 5px rgba(0,0,0, 0.3);
+    cursor: pointer;
   }
+
+  @media only screen and (max-width: 700px) {
+    row-gap: 1rem;
+    grid-template-columns: 1fr;
+    grid-template-areas: 
+    'two'
+    'one' 
+    'three'
+    'four';
+    
+    .info {
+      h3 {
+        text-align: center;
+      }
+      text-align: left;
+      bottom: 0rem;
+    }
+    img {
+      margin-bottom: 2.5rem;
+    }
+  }
+}
 `
 
 const images = [
@@ -116,33 +127,34 @@ function OtherProjects({ openModal, setOpenModal }) {
 
       <div style={{padding: '0 1rem', marginBottom: '2rem', textAlign: 'center', fontStyle: 'italic', marginTop: '1.35rem'}}>
             <p>Here are some other projects I did at school or following YouTube tutorials.</p>
-            <p>They range in different 
-              technologies I was interested in. Check them out!</p>
       </div>
 
         <ProjectDiv>
-          <div className='image image-left'>
-            <img src="/img/projects/other-projects/polished+.jpg" 
+
+          <div className='grid-item' style={{ gridArea: 'one' }}>
+            <img className='' src="/img/projects/other-projects/polished+.jpg" 
                 onClick={(e) => handleUrl("https://gleaming-kulfi-440d07.netlify.app/")}/>
           </div>
-          <div className='info text-right'>
-            <div className='info-container'>
+
+          <div className='grid-item' style={{ gridArea: 'two' }}>
+            <div className='info'>
               <h3 onClick={(e) => handleUrl("https://gleaming-kulfi-440d07.netlify.app/")}>Polished+</h3>
               <p>Beginning of college assignment I did, learning the fundamentals of HTML and CSS.</p>
             </div>
           </div>
-        </ProjectDiv>
         
-        <ProjectDiv>
-          <div className='info text-left'>
-            <div className='info-container'>
+          <div className='grid-item' style={{ gridArea: 'three' }}>
+            <div className='info left'>
               <h3 onClick={(e) => handleUrl("https://gleaming-kulfi-440d07.netlify.app/")}>Polished+</h3>
               <p>Beginning of college assignment I did, learning the fundamentals of HTML and CSS.</p>
-            </div>          </div>
-          <div className='image image-right'>
-            <img src="/img/projects/other-projects/polished+.jpg" 
+            </div>          
+          </div>
+
+          <div className='grid-item' style={{ gridArea: 'four' }}>
+            <img className='' src="/img/projects/other-projects/polished+.jpg" 
                 onClick={(e) => handleUrl("https://gleaming-kulfi-440d07.netlify.app/")}/>
           </div>
+
         </ProjectDiv>
 
       </div>
