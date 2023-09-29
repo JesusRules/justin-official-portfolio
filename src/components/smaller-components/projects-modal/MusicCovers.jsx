f14d07cce1ad9e78a3017ff66236e1d22b1eea96
Justin Bernard <justin.bernard320@gmail.com>
2023-09-29 02:56:08 -0400

    Should work now without grid-area

--- src/components/smaller-components/projects-modal/MusicCovers.jsx
+++ src/components/smaller-components/projects-modal/MusicCovers.jsx
@@ -29,6 +29,7 @@ const ContentContainer = styled.div`
 `;
 
 const ProjectDiv = styled.div`
+// SECTION SPACED
   display: grid;
   width: 100%;
   gap: .5rem;
@@ -38,16 +39,17 @@ const ProjectDiv = styled.div`
   row-gap: 3rem;
   align-items: center;
   grid-template-areas: 
-    'one two' 
+    /* 'one two' 
     'three four'
     'five six'
-    'seven eight'
-    'nine ten'
+    'seven eight' */
+    
+    /* 'nine ten'
     'eleven twelve'
     'thirteen fourteen'
     'fifteen sixteen'
     'seventeen eighteen'
-    'nineteen twenty'
+    'nineteen twenty' */
     ;
 
   margin-bottom: 1rem;
@@ -78,8 +80,27 @@ const ProjectDiv = styled.div`
       text-align: right;
     }
   }
-  .grid-item:nth-child(2n+1) {
+
+  .grid-item:nth-child(4n+3) {
     text-align: right;
+  }
+
+  // WORKS
+  .grid-item:nth-child(4n+1) {
+    grid-column-start: 1;
+    grid-column-end: 3;
+  }
+  .grid-item:nth-child(4n+2) {
+    grid-column-start: 3;
+    grid-column-end: 4;
+  }
+  .grid-item:nth-child(4n+3) {
+    grid-column-start: 0;
+    grid-column-end: 2;
+  }
+  .grid-item:nth-child(4n+4) {
+    grid-column-start: 2;
+    grid-column-end: 4;
   }
 
   // IMAGE 2
@@ -94,28 +115,56 @@ const ProjectDiv = styled.div`
 
   @media only screen and (max-width: 700px) {
     row-gap: 1rem;
+    display: flex;
+    flex-direction: column;
     grid-template-columns: 1fr;
-    grid-template-areas: 
-    'two'
-    'one'
-    'three'
-    'four'
-    'six'
-    'five'
-    'seven'
-    'eight'
-    'ten'
-    'nine'
-    'eleven'
-    'twelve'
-    'fourteen'
-    'thirteen'
-    'fifteen'
-    'sixteen'
-    'eighteen'
-    'seventeen'
-    'nineteen'
-    'twenty';
+    /* grid-template-areas: 
+    'item2'
+    'item1'
+    'item3'
+    'item4'
+    'item6'
+    'item5'
+    'item7'
+    'item8'
+    'item10'
+    'item9'
+    'item11'
+    'item12'
+    'item14'
+    'item13'
+    'item15'
+    'item16'
+    'item18'
+    'item19'
+    'item20'
+    'item21'; */
+
+    .grid-item:nth-child(1) {
+      order: 2;
+    }
+    .grid-item:nth-child(2) {
+      order: 1;
+    }
+
+    // WORKS
+  .grid-item:nth-child(4n+1) {
+    grid-column-start: 1;
+    grid-column-end: 1;
+  }
+  .grid-item:nth-child(4n+2) {
+    grid-column-start: 1;
+    grid-column-end: 1;
+  }
+  .grid-item:nth-child(4n+3) {
+    grid-column-start: 1;
+    grid-column-end: 1;
+  }
+  .grid-item:nth-child(4n+4) {
+    grid-column-start: 1;
+    grid-column-end: 1;
+  }
+    
     
     .grid-item {
       width: 100%;
@@ -150,8 +199,19 @@ const images = [
 ];
 
 function MusicCovers({ openModal, setOpenModal }) {
+  useEffect(() => {
+    AddGridAreaIncrementalNumbers();
+  }, [])
+
   const handleUrl = (stringLink) => {
     window.open(stringLink, '_blank');
+  }
+  
+  const AddGridAreaIncrementalNumbers = () => {
+    const gridItems = document.querySelectorAll('.grid-item');
+    // gridItems.forEach((item, index) => {
+    //   item.style.gridArea = `item${index + 1}`;
+    // });
   }
 
     return (
@@ -182,17 +242,17 @@ function MusicCovers({ openModal, setOpenModal }) {
       </div>
 
         <ProjectDiv>
-
-
           <div className='grid-item' 
-                  style={{ gridArea: 'one', 
-                          gridColumnStart: 1,
-                          gridColumnEnd: 3}}>
+                  style={{ 
+                          // gridArea: 'one',
+                          // gridColumnStart: 1,
+                          // gridColumnEnd: 3
+                          }}>
               <iframe src="https://www.youtube.com/embed/0QHEDCigeBw?si=3JZyK1nJkpJ29QOc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           </div>
-          <div className='grid-item' style={{ gridArea: 'two', 
-                                            gridColumnStart: 3,
-                                            gridColumnEnd: 4}}>
+          <div className='grid-item' style={{ 
+                                            // gridArea: 'two'
+                                            }}>
             <div className='info'>
               <h3 onClick={(e) => handleUrl("https://polished-plus.netlify.app/")}>Soundgarden<br/> - Beyond The Wheel</h3>
             </div>
@@ -203,16 +263,16 @@ function MusicCovers({ openModal, setOpenModal }) {
 
 
 
-          <div className='grid-item' style={{ gridArea: 'three', 
-                                            gridColumnStart: 0,
-                                            gridColumnEnd: 2}}>
+          <div className='grid-item' style={{ 
+                                          // gridArea: 'three'
+                                        }}>
             <div className='info'>
               <h3 onClick={(e) => handleUrl("https://polished-plus.netlify.app/")}>Soundgarden<br/> - Beyond The Wheel</h3>
             </div>
           </div>
-          <div className='grid-item' style={{ gridArea: 'four', 
-                                            gridColumnStart: 2,
-                                            gridColumnEnd: 4}}>
+          <div className='grid-item' style={{ 
+                                              // gridArea: 'four'
+                                            }}>
               <iframe src="https://www.youtube.com/embed/0QHEDCigeBw?si=3JZyK1nJkpJ29QOc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           </div>
 
@@ -221,15 +281,14 @@ function MusicCovers({ openModal, setOpenModal }) {
 
 
 
-          <div className='grid-item' 
+          {/* <div className='grid-item' 
                   style={{ gridArea: 'five', 
-                          gridColumnStart: 1,
-                          gridColumnEnd: 3}}>
+                          // gridColumnStart: 1,
+                          // gridColumnEnd: 3
+                          }}>
               <iframe src="https://www.youtube.com/embed/0QHEDCigeBw?si=3JZyK1nJkpJ29QOc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           </div>
-          <div className='grid-item' style={{ gridArea: 'six', 
-                                            gridColumnStart: 3,
-                                            gridColumnEnd: 4}}>
+          <div className='grid-item' style={{ gridArea: 'six'}}>
             <div className='info'>
               <h3 onClick={(e) => handleUrl("https://polished-plus.netlify.app/")}>Soundgarden<br/> - Beyond The Wheel</h3>
             </div>
@@ -239,18 +298,16 @@ function MusicCovers({ openModal, setOpenModal }) {
 
 
 
-          <div className='grid-item' style={{ gridArea: 'seven', 
-                                            gridColumnStart: 0,
-                                            gridColumnEnd: 2}}>
+          <div className='grid-item' style={{ gridArea: 'seven'}}>
             <div className='info'>
               <h3 onClick={(e) => handleUrl("https://polished-plus.netlify.app/")}>Soundgarden<br/> - Beyond The Wheel</h3>
             </div>
           </div>
-          <div className='grid-item' style={{ gridArea: 'eight', 
-                                            gridColumnStart: 2,
-                                            gridColumnEnd: 4}}>
+          <div className='grid-item' style={{ gridArea: 'eight'}}>
               <iframe src="https://www.youtube.com/embed/0QHEDCigeBw?si=3JZyK1nJkpJ29QOc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          </div>
+          </div> */}
+          
+
           
 
 
